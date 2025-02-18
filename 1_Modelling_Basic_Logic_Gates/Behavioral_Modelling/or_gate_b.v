882941b473f50ae30d1c75d2357a24f234399dff
Jayakrishnan Menon <jkmenon18@gmail.com>
2025-02-18 19:52:20 +0530

    Create or_gate_b.v

---
+++ 1_Modelling_Basic_Logic_Gates/Behavioral_Modelling/or_gate_b.v
@@ -0,0 +1,9 @@
+//OR Gate
+module or_gate_b(y,a,b);
+input a,b;
+output reg y;
+always @(a or b)begin
+if(a==1'b0 & b==1'b0) begin y = 1'b0; end
+else                        y = 1'b1;
+end
+endmodule
