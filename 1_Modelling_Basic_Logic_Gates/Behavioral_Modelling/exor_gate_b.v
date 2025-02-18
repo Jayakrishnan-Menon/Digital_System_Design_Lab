3849b5914480bcc04c040e2118f7a582a3723e12
Jayakrishnan Menon <jkmenon18@gmail.com>
2025-02-18 19:54:22 +0530

    Create exor_gate_b.v

---
+++ 1_Modelling_Basic_Logic_Gates/Behavioral_Modelling/exor_gate_b.v
@@ -0,0 +1,11 @@
+//EXOR Gate:
+module exor_gate_b(y,a,b);
+input a,b;
+output reg y;
+always @(a or b)begin
+if(a==1'b0 & b==1'b0) begin y = 1'b0; end
+if(a==1'b1 & b==1'b1) begin y = 1'b0; end
+if(a==1'b0 & b==1'b1) begin y = 1'b1; end
+if(a==1'b1 & b==1'b0) begin y = 1'b1; end
+end
+endmodule
