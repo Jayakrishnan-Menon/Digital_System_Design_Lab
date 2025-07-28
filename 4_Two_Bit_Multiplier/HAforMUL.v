0518435ea779002a703401a03f2c6090e7bf4c99
Jayakrishnan Menon <jkmenon18@gmail.com>
2025-07-28 12:27:31 +0530

    Update HAforMUL.v

--- 4_Two_Bit_Multiplier/HAforMUL.v
+++ 4_Two_Bit_Multiplier/HAforMUL.v
@@ -1,7 +1,7 @@
 //Half Adder for multiplier
 module HAforMUL(a,b,s,co);
-input a,b;
-output s,co;
-xor x1(s,a,b);
-and a1(co,a,b);
+  input a,b;
+  output s,co;
+  xor x1(s,a,b);
+  and a1(co,a,b);
 endmodule 
