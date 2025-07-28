33c26438beddf469a10462f3b6840473d4bc05e9
Jayakrishnan Menon <jkmenon18@gmail.com>
2025-07-28 11:47:40 +0530

    Create full_sub_gatelvl.v

---
+++ 2_Adders_And_Subtractors/Full_Subtractor/full_sub_gatelvl.v
@@ -0,0 +1,13 @@
+//gatelevel
+module full_sub_gatelvl(
+    input a,b,bi,
+    output d,bo);
+wire x1,x2,x3,x4;
+xor (x1,a,b);
+not (x2,x1);
+not (x3,a);
+and (x4,x3,b);
+xor (d,x1,bi);
+and (x5,x2,bi);
+or (bo,x5,x4);
+endmodule
