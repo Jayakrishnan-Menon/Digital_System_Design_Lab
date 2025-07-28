6ff29bc22e1632745dfb9871ce866b07aaaba764
Jayakrishnan Menon <jkmenon18@gmail.com>
2025-07-28 12:23:35 +0530

    Update FAforPA.v

--- 3_Four_Bit_Parallel_Adder/FAforPA.v
+++ 3_Four_Bit_Parallel_Adder/FAforPA.v
@@ -1,12 +1,12 @@
 //Full Adder module
 module FAforPA(a,b,cin,sum,cout);
-input a,b,cin;
-output wire sum,cout;
-wire s1,c1,c2,c3;
-xor(s1,a,b);
-xor(sum,s1,cin);
-and(c1,a,b);
-and(c2,b,cin);
-and(c3,a,cin);
-or(cout,c1,c2,c3);
+  input a,b,cin;
+  output wire sum,cout;
+  wire s1,c1,c2,c3;
+  xor(s1,a,b);
+  xor(sum,s1,cin);
+  and(c1,a,b);
+  and(c2,b,cin);
+  and(c3,a,cin);
+  or(cout,c1,c2,c3);
 endmodule
