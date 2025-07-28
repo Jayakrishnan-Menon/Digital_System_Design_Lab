7acc641ae0b58164c5267170b14c2af8e8745938
Jayakrishnan Menon <jkmenon18@gmail.com>
2025-07-28 12:13:00 +0530

    Update nand_gate_b.v

--- 1_Modelling_Basic_Logic_Gates/Behavioral_Modelling/nand_gate_b.v
+++ 1_Modelling_Basic_Logic_Gates/Behavioral_Modelling/nand_gate_b.v
@@ -1,8 +1,9 @@
 //NAND Gate
 module nand_gate_b(y,a,b);
-input a,b;
-output reg y;
-always@(a or b) begin
-if(a==1'b1 & b==1'b1) begin y=1'b0; end
-else                        y=1'b1; end
+  input a,b;
+  output reg y;
+  always@(a or b) begin
+    if(a==1'b1 & b==1'b1) begin y=1'b0; end
+    else                        y=1'b1; end
+  end
 endmodule
