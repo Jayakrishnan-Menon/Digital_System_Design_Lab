377b80d1d71d0c3ebf4ec7b0c61c42daa0fd45e4
Jayakrishnan Menon <jkmenon18@gmail.com>
2025-07-28 11:56:46 +0530

    Create SR_FF.v

---
+++ 5_Flip_Flops/SR/SR_FF.v
@@ -0,0 +1,20 @@
+//SR Flip Flop Verilog code
+module SR_FF(Q,QB,S,R,CLK); 
+input S,R,CLK; 
+output Q,QB; 
+reg Q,QB; 
+initial begin 
+Q=1'b0;
+QB=1'b1;
+end
+always @(posedge CLK) 
+begin 
+case({S,R}) 
+2'b00:Q=Q; 
+2'b01:Q=0; 
+2'b10:Q=1; 
+2'b11:Q=1'bx; 
+endcase
+QB=~Q;
+end
+endmodule
