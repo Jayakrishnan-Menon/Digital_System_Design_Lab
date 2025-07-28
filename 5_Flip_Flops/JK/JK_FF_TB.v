40c0855c67207e32a14ae240801f6bb4aeb1769c
Jayakrishnan Menon <jkmenon18@gmail.com>
2025-07-28 11:58:55 +0530

    Create JK_FF_TB.v

---
+++ 5_Flip_Flops/JK/JK_FF_TB.v
@@ -0,0 +1,17 @@
+//JK Flip Flop Testbench code
+module JK_FF_TB; 
+reg J;
+reg K;
+reg CLK;
+wire Q; 
+wire QB;
+JK_FF uut ( .Q(Q), .QB(QB), .J(J), .K(K), .CLK(CLK));
+always #100 CLK=~CLK; 
+initial begin 
+CLK=1; 
+#200 J=1;K=0;
+#200 J=0; K=0;
+#200 J=0; K=1;
+#200 J=1; K=1;
+end
+endmodule
