74c71468a1e73fc730230aecdab0d7d95c9bcdc3
Jayakrishnan Menon <jkmenon18@gmail.com>
2025-07-28 12:10:53 +0530

    Update and_gate_b.v

--- 1_Modelling_Basic_Logic_Gates/Behavioral_Modelling/and_gate_b.v
+++ 1_Modelling_Basic_Logic_Gates/Behavioral_Modelling/and_gate_b.v
@@ -1,9 +1,9 @@
 //AND Gate
 module and_gate_b(Y, A, B);
-input A,B;
-output reg Y;
-always @ (A or B) begin
-if (A == 1'b1 & B == 1'b1) begin Y = 1'b1; end
-else                       begin Y = 1'b0; end
-end
+  input A,B;
+  output reg Y;
+  always @ (A or B) begin
+    if (A == 1'b1 & B == 1'b1) begin Y = 1'b1; end
+    else                       begin Y = 1'b0; end
+  end
 endmodule
