a3cfaa8552e46f2c953caea1366754bae6db1cf1
Jayakrishnan Menon <jkmenon18@gmail.com>
2025-07-28 12:20:32 +0530

    Update full_sub_tb.v

--- 2_Adders_And_Subtractors/Full_Subtractor/full_sub_tb.v
+++ 2_Adders_And_Subtractors/Full_Subtractor/full_sub_tb.v
@@ -1,17 +1,16 @@
 //common testbench
 module full_sub_tb;
-reg a,b,bi;
-wire d,bo;
-full_sub dut(.a(a), .b(b), .bi(bi), .d(d), .bo(bo));
-initial
-begin
-a=0; b=0; bi=0; #100;
-a=0; b=0; bi=1; #100;
-a=0; b=1; bi=0; #100;
-a=0; b=1; bi=1; #100;
-a=1; b=0; bi=0; #100;
-a=1; b=0; bi=1; #100;
-a=1; b=1; bi=0; #100;
-a=1; b=1; bi=1; #100;
-end
+  reg a,b,bi;
+  wire d,bo;
+  full_sub dut(.a(a), .b(b), .bi(bi), .d(d), .bo(bo));
+  initial begin
+    a=0; b=0; bi=0; #100;
+    a=0; b=0; bi=1; #100;
+    a=0; b=1; bi=0; #100;
+    a=0; b=1; bi=1; #100;
+    a=1; b=0; bi=0; #100;
+    a=1; b=0; bi=1; #100;
+    a=1; b=1; bi=0; #100;
+    a=1; b=1; bi=1; #100;
+  end
 endmodule
