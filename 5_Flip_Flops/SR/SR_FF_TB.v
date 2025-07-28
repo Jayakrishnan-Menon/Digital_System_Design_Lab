e26d41f32fd0ea40fdd02eacba4b13278dbb3454
Jayakrishnan Menon <jkmenon18@gmail.com>
2025-07-28 12:34:01 +0530

    Update SR_FF_TB.v

--- 5_Flip_Flops/SR/SR_FF_TB.v
+++ 5_Flip_Flops/SR/SR_FF_TB.v
@@ -1,15 +1,16 @@
 //SR Flip Flop Testbench code
 module SR_FF_TB;
-reg S; reg R; 
-reg CLK; 
-wire Q; wire QB;
-SR_FF uut ( .Q(Q), .QB(QB), .S(S), .R(R), .CLK(CLK));
-always #100 CLK=~CLK; 
-initial begin 
-CLK=1;
-#200 S=1; R=0; 
-#200 S=0; R=0;
-#200 S=0; R=1; 
-#200 S=1; R=1;
-end 
+  reg S; reg R; 
+  reg CLK; 
+  wire Q; 
+  wire QB;
+  SR_FF uut ( .Q(Q), .QB(QB), .S(S), .R(R), .CLK(CLK));
+  always #100 CLK=~CLK; 
+  initial begin 
+    CLK=1;
+    #200 S=1; R=0; 
+    #200 S=0; R=0;
+    #200 S=0; R=1; 
+    #200 S=1; R=1;
+  end 
 endmodule
