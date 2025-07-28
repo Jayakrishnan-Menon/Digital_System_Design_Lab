583cd73790ae5881cf8007c33d42f16bf1a20704
Jayakrishnan Menon <jkmenon18@gmail.com>
2025-07-28 12:21:39 +0530

    Update half_add_tb.v

--- 2_Adders_And_Subtractors/Half_Adder/half_add_tb.v
+++ 2_Adders_And_Subtractors/Half_Adder/half_add_tb.v
@@ -1,13 +1,12 @@
 //common test bench
 module half_add_tb;
-reg a,b;
-wire c,s;
-half_add dut(.a(a), .b(b), .c(c), .s(s));
-initial
-begin
-a=0; b=0; #100;
-a=0; b=1; #100;
-a=1; b=0; #100;
-a=1; b=1; #100;
-end
+  reg a,b;
+  wire c,s;
+  half_add dut(.a(a), .b(b), .c(c), .s(s));
+  initial begin
+    a=0; b=0; #100;
+    a=0; b=1; #100;
+    a=1; b=0; #100;
+    a=1; b=1; #100;
+  end
 endmodule
