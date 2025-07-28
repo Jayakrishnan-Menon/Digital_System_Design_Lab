59a23f489d137ac6523b33c04332c1a00d4b6b59
Jayakrishnan Menon <jkmenon18@gmail.com>
2025-07-28 12:35:38 +0530

    Update T_FF_TB.v

--- 5_Flip_Flops/T/T_FF_TB.v
+++ 5_Flip_Flops/T/T_FF_TB.v
@@ -1,16 +1,16 @@
 //T Flip Flop Testbench code
 module T_FF_TB; 
-reg T; 
-reg CLK; 
-wire Q;
-wire QB;
-T_FF uut ( .Q(Q), .QB(QB), .T(T), .CLK(CLK));
-always #100 CLK=~CLK; 
-initial begin 
-CLK=1; 
-#200 T=0; 
-#200 T=1;
-#200 T=0;
-#200 T=1;
-end
+  reg T; 
+  reg CLK; 
+  wire Q;
+  wire QB;
+  T_FF uut ( .Q(Q), .QB(QB), .T(T), .CLK(CLK));
+  always #100 CLK=~CLK; 
+  initial begin 
+    CLK=1; 
+    #200 T=0; 
+    #200 T=1;
+    #200 T=0;
+    #200 T=1;
+  end
 endmodule
