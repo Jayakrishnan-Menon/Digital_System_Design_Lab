2e271cc192822c82a2e95f68ca68530c99663f31
Jayakrishnan Menon <jkmenon18@gmail.com>
2025-07-28 12:07:13 +0530

    Create dff.v

---
+++ 6_Ripple_Counter/dff.v
@@ -0,0 +1,13 @@
+//D flip flop for Ripple Counter
+module dff (input d,
+ input clk,
+ input rstn,
+ output reg q,
+ output qn);
+ always @ (posedge clk or negedge rstn)
+ if (!rstn)
+ q <= 0;
+ else
+ q <= d;
+ assign qn = ~q;
+endmodule
