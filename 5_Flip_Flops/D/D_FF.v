2ccfeabd50d66ea76b21eec60d2becdc1fa001d3
Jayakrishnan Menon <jkmenon18@gmail.com>
2025-07-28 12:29:48 +0530

    Update D_FF.v

--- 5_Flip_Flops/D/D_FF.v
+++ 5_Flip_Flops/D/D_FF.v
@@ -1,15 +1,14 @@
 //D Flip Flop Verilog code
 module D_FF(Q,QB,D,CLK); 
-input D,CLK; 
-output Q,QB; 
-reg Q,QB; 
-initial begin 
-Q=1'b0;
-QB=1'b1;
-end
-always @(posedge CLK) 
-begin 
-Q=D; 
-QB=~Q; 
-end
+  input D,CLK; 
+  output Q,QB; 
+  reg Q,QB; 
+  initial begin 
+    Q=1'b0;
+    QB=1'b1;
+  end
+  always @(posedge CLK) begin
+    Q=D; 
+    QB=~Q;
+  end
 endmodule
