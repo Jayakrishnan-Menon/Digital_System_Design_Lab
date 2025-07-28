cc4e74422d0b0400cf76de6671623ec9d1caa8e5
Jayakrishnan Menon <jkmenon18@gmail.com>
2025-07-28 12:20:52 +0530

    Update half_add_dataflow.v

--- 2_Adders_And_Subtractors/Half_Adder/half_add_dataflow.v
+++ 2_Adders_And_Subtractors/Half_Adder/half_add_dataflow.v
@@ -1,7 +1,7 @@
 //dataflow modelling
 module half_add_dataflow(a,b,c,s);
-input a,b;
-output c,s;
-assign s = a^b;
-assign c = a&b;
+  input a,b;
+  output c,s;
+  assign s = a^b;
+  assign c = a&b;
 endmodule
