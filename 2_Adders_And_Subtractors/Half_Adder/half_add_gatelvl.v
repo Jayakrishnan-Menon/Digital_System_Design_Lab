17d79bb78726471ddd4ff4323ae7ce1faf50528f
Jayakrishnan Menon <jkmenon18@gmail.com>
2025-07-28 12:21:09 +0530

    Update half_add_gatelvl.v

--- 2_Adders_And_Subtractors/Half_Adder/half_add_gatelvl.v
+++ 2_Adders_And_Subtractors/Half_Adder/half_add_gatelvl.v
@@ -1,7 +1,7 @@
 //gate level modelling
 module half_add_gatelvl(a,b,c,s);
-input a,b;
-output c,s;
-xor (s,a,b);
-and (c,a,b);
+  input a,b;
+  output c,s;
+  xor (s,a,b);
+  and (c,a,b);
 endmodule
