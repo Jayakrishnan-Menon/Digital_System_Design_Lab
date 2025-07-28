1f68e34fcd9126c5552171e56ca4fa494f0c3f6a
Jayakrishnan Menon <jkmenon18@gmail.com>
2025-07-28 12:38:24 +0530

    Update dff.v

--- 6_Ripple_Counter/dff.v
+++ 6_Ripple_Counter/dff.v
@@ -1,13 +1,8 @@
 //D flip flop for Ripple Counter
-module dff (input d,
- input clk,
- input rstn,
- output reg q,
- output qn);
- always @ (posedge clk or negedge rstn)
- if (!rstn)
- q <= 0;
- else
- q <= d;
+module dff (input d, input clk, input rstn, output reg q, output qn);
+ always @ (posedge clk or negedge rstn) begin
+  if (!rstn) q <= 0;
+  else       q <= d;
+ end
  assign qn = ~q;
 endmodule
