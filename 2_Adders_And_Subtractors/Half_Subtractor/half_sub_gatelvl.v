6de77c69359eb58ca80c6bf6f6a1738ef7a6e1fa
Jayakrishnan Menon <jkmenon18@gmail.com>
2025-07-28 12:22:33 +0530

    Update half_sub_gatelvl.v

--- 2_Adders_And_Subtractors/Half_Subtractor/half_sub_gatelvl.v
+++ 2_Adders_And_Subtractors/Half_Subtractor/half_sub_gatelvl.v
@@ -1,9 +1,7 @@
 //gatelevel
-module half_sub_gatelvl(
-    input a, b,
-    output d, bo);
-wire x;
-xor (d,a,b);
-not (x,a);
-and (bo,x,b);
+module half_sub_gatelvl(input a, b, output d, bo);
+    wire x;
+    xor (d,a,b);
+    not (x,a);
+    and (bo,x,b);
 endmodule
