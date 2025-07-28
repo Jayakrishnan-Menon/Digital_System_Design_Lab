be8ae9fb39c9b88221759a600dd88c5010f61c6b
Jayakrishnan Menon <jkmenon18@gmail.com>
2025-07-28 12:22:08 +0530

    Update half_sub_dataflow.v

--- 2_Adders_And_Subtractors/Half_Subtractor/half_sub_dataflow.v
+++ 2_Adders_And_Subtractors/Half_Subtractor/half_sub_dataflow.v
@@ -1,7 +1,5 @@
 //dataflow
-module half_sub_dataflow(
-    input a, b,
-    output d, bo);
-assign d = a^b;
-assign bo = ((~a)&b);
+module half_sub_dataflow(input a, b, output d, bo);
+    assign d = a^b;
+    assign bo = ((~a)&b);
 endmodule
