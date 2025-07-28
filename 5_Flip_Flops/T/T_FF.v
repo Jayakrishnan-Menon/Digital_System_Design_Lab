0e2f27f950685e9481b9e0484c2e315eedd233b7
Jayakrishnan Menon <jkmenon18@gmail.com>
2025-07-28 12:35:01 +0530

    Update T_FF.v

--- 5_Flip_Flops/T/T_FF.v
+++ 5_Flip_Flops/T/T_FF.v
@@ -1,18 +1,17 @@
 //T Flip Flop Verilog code
 module T_FF(Q,QB,T,CLK);
-input T,CLK; 
-output Q,QB; 
-reg Q=0,QB;
-initial begin 
-Q=1'b0;
-QB=1'b1;
-end
-always @(posedge CLK)
-begin
-case(T)
-1'b0:Q=Q; 
-1'b1:Q=~Q;
-endcase
-QB=~Q;
-end 
+  input T,CLK; 
+  output Q,QB; 
+  reg Q=0,QB;
+  initial begin 
+    Q=1'b0;
+    QB=1'b1;
+  end
+  always @(posedge CLK) begin
+    case(T)
+      1'b0:Q=Q; 
+      1'b1:Q=~Q;
+    endcase
+    QB=~Q;
+  end 
 endmodule
